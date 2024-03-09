b9e963d00ad06576b33982f54c00adf35fb948ec
Amit Prakash <amitprakash067@gmail.com>
2024-03-09 13:24:38 +0530

    netflix clone

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCy_JE0lNXjDhFQfgGgGPPtZ6hA2q0Wv7I",
+  authDomain: "netflixgpt-98ba2.firebaseapp.com",
+  projectId: "netflixgpt-98ba2",
+  storageBucket: "netflixgpt-98ba2.appspot.com",
+  messagingSenderId: "779112537655",
+  appId: "1:779112537655:web:72c7ba0976de3d1e77a9b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
